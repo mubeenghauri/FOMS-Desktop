c953de2756e0fe802cf61f17db57bec63b906530
mubeenghauri <p176107@nu.edu.pk>
2019-11-24 01:49:15 +0500

    Added ApproveButton Class.

---
+++ src/main/java/home/models/lib/ApproveButton.java
@@ -0,0 +1,25 @@
+package home.models.lib;
+
+import javafx.scene.control.Button;
+
+/**
+ * ApproveButton : Each row in AcceptNewOrder shall a column of ApproveButton
+ * each button representing a unique order, onclick shall mark the respective
+ * order as "Approved" and the said order will be saved as "Accepted Order"
+ *
+ * @author mubeenghauri
+ */
+
+public class ApproveButton extends Button {
+    private String ButtonId;
+
+    public ApproveButton(String id) {
+        this.ButtonId = id;
+
+        this.setText("Approve?");
+        setOnAction((event) -> {
+            System.out.println("Approved Order with id : "+this.ButtonId);
+            this.setText("Approved!");
+        });
+    }
+}
