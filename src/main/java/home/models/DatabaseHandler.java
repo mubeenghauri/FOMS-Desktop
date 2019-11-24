ade5f26ab6fb602990994716b34cca46c48ebeb7
mubeenghauri <p176107@nu.edu.pk>
2019-11-24 23:03:19 +0500

    Updated DatabaseHandler, orders are being added to database when recieved

--- src/main/java/home/models/DatabaseHandler.java
+++ src/main/java/home/models/DatabaseHandler.java
@@ -15,4 +15,22 @@ public class DatabaseHandler {
          return st.executeQuery(qry);
     }
 
+    public void addOrderToDb(Order o) throws SQLException, ClassNotFoundException {
+        System.out.println("[DatabaseHandler::addOrderToDb] [NOTE] Recieved order : "+o.getOrderNumber());
+        String qry = "insert into allOrders(ORDERID, CUSTOMERNAME, PHONE, ADDRESS, ITEMS, BILLAMMOUNT) values ( '"
+                + o.getOrderNumber()      + "', '"
+                + o.getCustomerName()     + "', '"
+                + "0300-1231231"          + "', '"
+                + o.getAddressToDeliver() + "', '"
+                + o.getItems()            + "', "
+                + "0"                  + "  "
+                + ")";
+        System.out.println("[DatabaseHandler::addOrderToDb] [NOTE]  Running query  : "+qry);
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/foodo","mubeen","");
+        Statement st = con.createStatement();
+        st.executeUpdate(qry);
+    }
+
 }
