94290d7de8ab57fc8fac2cde101e4f064be06ee0
mubeenghauri <p176107@nu.edu.pk>
2019-11-25 02:10:14 +0500

    Added addOrderToAccept() accept button now working

--- src/main/java/home/models/lib/ApproveButton.java
+++ src/main/java/home/models/lib/ApproveButton.java
@@ -1,7 +1,11 @@
 package home.models.lib;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import home.models.DatabaseHandler;
+import home.models.Operator;
 import javafx.scene.control.Button;
+
+import java.sql.SQLException;
 
 /**
  * ApproveButton : Each row in AcceptNewOrder shall a column of ApproveButton
@@ -13,14 +17,23 @@ import javafx.scene.control.Button;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ApproveButton extends Button {
     private String ButtonId;
+    private DatabaseHandler dbHandler;
 
     public ApproveButton(String id) {
         this.ButtonId = id;
-
+        this.dbHandler = new DatabaseHandler();
         this.setText("Approve?");
         setOnAction((event) -> {
-            System.out.println("Approved Order with id : "+this.ButtonId);
+            System.out.println("Approved Order operator id"+ Operator.getId() + " with id : "+this.ButtonId);
+            try {
+                dbHandler.addOrderToAccepted(this.ButtonId, Operator.getId());
+
+            } catch (Exception e) {
+               System.out.println("[ApproveButton] [ERROR] DatabaseHandler failed to add order to accepted table");
+            }
             this.setText("Approved!");
         });
     }
+
+
 }
