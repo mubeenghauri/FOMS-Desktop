d5b1d7d515b381408a97d0ff27b6037c66c39150
mubeenghauri <p176107@nu.edu.pk>
2019-11-24 22:15:17 +0500

    Updated DatabaseHandler

--- src/main/java/home/models/DatabaseHandler.java
+++ src/main/java/home/models/DatabaseHandler.java
@@ -8,7 +8,8 @@ public class DatabaseHandler {
 
 
      ResultSet getResult(String qry) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+         System.out.println("[DatabaseHandler::getResult] [NOTE] Recieved query : "+qry);
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/foodo","mubeen","");
          Statement st = con.createStatement();
          return st.executeQuery(qry);
