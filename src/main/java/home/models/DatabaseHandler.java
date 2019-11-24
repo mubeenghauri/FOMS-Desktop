97c5c362066e5506dd08a36091b9fd4f62cd1443
mubeenghauri <p176107@nu.edu.pk>
2019-11-24 23:09:40 +0500

    Updated DatabaseHandler

--- src/main/java/home/models/DatabaseHandler.java
+++ src/main/java/home/models/DatabaseHandler.java
@@ -1,16 +1,13 @@
 package home.models;
 
-//import java.sql.*;
 import java.sql.*;
-
 
 public class DatabaseHandler {
 
-
      ResultSet getResult(String qry) throws ClassNotFoundException, SQLException {
          System.out.println("[DatabaseHandler::getResult] [NOTE] Recieved query : "+qry);
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/foodo","mubeen","");
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/foodo","mubeen","");
          Statement st = con.createStatement();
          return st.executeQuery(qry);
     }
@@ -23,14 +20,12 @@ public class DatabaseHandler {
                 + "0300-1231231"          + "', '"
                 + o.getAddressToDeliver() + "', '"
                 + o.getItems()            + "', "
-                + "0"                  + "  "
+                + "0"                     + "  "
                 + ")";
         System.out.println("[DatabaseHandler::addOrderToDb] [NOTE]  Running query  : "+qry);
-
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/foodo","mubeen","");
         Statement st = con.createStatement();
         st.executeUpdate(qry);
     }
-
 }
